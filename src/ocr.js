7c12aac1321faf87022db043afc315c6cc3b7f3a
qinghe <david@getstore.cn>
2019-07-04 14:18:09 +0800

    improve ocr

--- src/ocr.js
+++ src/ocr.js
@@ -15,7 +15,7 @@ async function getVerifyCode( driver ){
   let buffer = new Buffer.from(encoded, 'base64');
   //let binaryString = buff.toString('binary');
   let codeBuffer = await sharp(buffer).extract({ width: rect.width, height: rect.height, left: rect.x, top: rect.y }).toBuffer()
-  let code = await Tesseract.recognize(codeBuffer)
+  let code = await Tesseract.recognize(codeBuffer, {tessedit_char_whitelist: '0123456789', tessedit_create_hocr: '0', tessedit_create_tsv: '0' })
   return code
 }
 
