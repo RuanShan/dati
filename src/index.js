970764ed882cc7e4c36feafd8af615c70ec794b2
qinghe <david@getstore.cn>
2019-11-26 10:47:11 +0800

    删除无用文件，更新dati.exe

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // import util from './src/util.js'
+const fs = require('fs');
+
 const {
   scrollToBottom,
   playVideo
@@ -16,6 +18,29 @@ const {
   until
 } = require('selenium-webdriver');
 
+// 为课程代码创建数据库
+async function handleCreateDb(couseCodes, username, password ) {
+  if( !username || !password){
+    throw  new Error( "用户名和密码是必须的")
+  }
+
+  let driver = await new Builder().forBrowser('chrome').build();
+  let bot = new Bot(driver)
+  console.log(" bot doing profile a couse")
+    // 1934001474084
+    // 19930902
+  await bot.login(username, password)
+  await bot.prepareForLearn()
+  let userInfo = { username, couses:[] }
+  for( let i=0; i<couseCodes.length; i++){
+    let couseCode = couseCodes[i]
+    await bot.profileCouse(couseCode)
+    userInfo.couses.push( bot.couseInfo.score )
+  }
+  await saveUserJson( username, userInfo )
+  await driver.quit()
+}
+
 async function handleCreateLog(couseCode, username, password ) {
   if( !username || !password){
     throw  new Error( "用户名和密码是必须的")
@@ -29,7 +54,28 @@ async function handleCreateLog(couseCode, username, password ) {
   await bot.login(username, password)
   await bot.prepareForLearn(couseCode)
   await bot.profileCouse(couseCode)
-  driver.close()
+  await driver.quit()
+}
+
+// 学习多门课程
+async function handleLearnCourses(couseCodes, username, password) {
+  if( !username || !password){
+    throw  new Error( "用户名和密码是必须的")
+  }
+
+  let driver = await new Builder().forBrowser('chrome').build();
+  let bot = new Bot(driver)
+  console.log(" bot doing handleLearnCourse")
+
+  await bot.login(username, password)
+  await bot.prepareForLearn()
+  for( let i=0; i<couseCodes.length; i++){
+    let couseCode = couseCodes[i]
+    await bot.getLog(username, couseCode)
+    await bot.learnCourse()
+  }
+
+  await driver.quit()
 }
 
 async function handleLearnCourse(couseCode, username, password) {
@@ -45,7 +91,7 @@ async function handleLearnCourse(couseCode, username, password) {
   await bot.getLog(username, couseCode)
   await bot.prepareForLearn(couseCode)
   await bot.learnCourse()
-  driver.close()
+  await driver.quit()
 }
 
 async function handleLearnByCodeModule(couseCode, moduleCode,username, password) {
@@ -58,12 +104,21 @@ async function handleLearnByCodeModule(couseCode, moduleCode,username, password)
   await bot.getLog(username, couseCode)
   await bot.prepareForLearn(couseCode)
   await bot.learnModule(moduleCode)
-  driver.close()
+  await driver.quit()
 }
 
+async function saveUserJson(username, userInfo) {
+  let filename =  './db/' + username  + '.json'
+  fs.writeFile(filename, JSON.stringify(userInfo), (err) => {
+    if (err) throw err;
+    console.log(`文件已被保存:${filename}`);
+  });
+}
 
 module.exports = {
+  handleCreateDb,
   handleCreateLog,
   handleLearnCourse,
+  handleLearnCourses,
   handleLearnByCodeModule
 }
